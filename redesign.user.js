bd2219e74230ae20fc1bee994a03754a7c92c0ef
Jan <jan.boehmerle@ledbrain.de>
2024-09-06 12:31:15 +0200

    fix(settings): fix settings buttons not being clickable partially (#370)
    * fix(settings): fix settings buttons not being clickable partially
    
    * chore(main): release 1.40.1 (#369)
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- redesign.user.js
+++ redesign.user.js
@@ -2,7 +2,7 @@
 // @name            🎓️ moodlemoot DACH: better-moodle
 // @namespace       https://mmd.better-moodle.dev
 // @                x-release-please-start-version
-// @version         1.40.0
+// @version         1.40.1
 // @                x-release-please-end
 // @author          Jan (jxn_30), Yorik (YorikHansen)
 // @description     Improves Moodle by cool features and design improvements.
@@ -7537,6 +7537,7 @@ ready(() => {
                     top: 5%;
                     left: 5%;
                     object-fit: contain;
+                    pointer-events: none;
                 }
 
                 @media (prefers-contrast: more) {
